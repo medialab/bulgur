f64af5d8c6a4692a1396b7ef133a7c4b7fab3b61
Robin de Mourat <robin.demourat@gmail.com>
2016-10-06 11:37:13 +0200

    connect to redux dev tools

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import Application from './Application';
 
 let CurrentApplication = Application;
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 window.store = store;
 
 const mountNode = document.getElementById('mount');
