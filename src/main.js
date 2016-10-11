14de03f43fa8cf30acfe1fd5152f6acf98fb6b2d
Robin de Mourat <robin.demourat@gmail.com>
2016-10-11 10:59:35 +0200

    refactor quinoa plugging in an external module

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux';
 
 import configureStore from './redux/configureStore';
 import Application from './Application';
-import { initQuinoa } from './helpers/configQuinoa';
+import {initQuinoa} from './helpers/configQuinoa';
 
 let CurrentApplication = Application;
 
