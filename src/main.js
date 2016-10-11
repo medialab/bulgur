342a340369720444d268751e7be2c63fbdaedfa0
Robin de Mourat <robin.demourat@gmail.com>
2016-10-11 09:28:14 +0200

    plug quinoa logic

--- src/main.js
+++ src/main.js
@@ -6,20 +6,79 @@
  */
 import React from 'react';
 import {render} from 'react-dom';
-// import Quinoa from 'quinoa';
+import Quinoa from 'quinoa';
 import {Provider} from 'react-redux';
+import uuid from 'uuid';
+import {EditorState} from 'draft-js';
 
 import configureStore from './redux/configureStore';
-// import reducers from './redux/rootReducer';
 import Application from './Application';
 
 let CurrentApplication = Application;
 
-const store = configureStore();/*createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );*/
+const store = configureStore();
 window.store = store;
+
+
+// Init quinoa
+function createSlide(data) {
+  return {
+    id: uuid.v4(),
+    title: data.title || '',
+    markdown: data.markdown || '',
+    draft: EditorState.createEmpty(),
+    meta: {
+      graph: 'arctic',
+      camera: {
+        x: 0,
+        y: 0,
+        angle: 0,
+        ratio: 1
+      }
+    }
+  };
+}
+
+/**
+ * Create an editor state object from slides & resources.
+ *
+ * @param  {array}  slides - Slides list.
+ * @return {object}        - Editor state object.
+ */
+function createEditorState(slides = []) {
+  const slidesMap = {};
+
+  slides.forEach(slide => (slidesMap[slide.id] = slide));
+
+  return {
+    current: slides[0].id,
+    slides: slidesMap,
+    order: slides.map(slide => slide.id)
+  };
+}
+
+/**
+ * Create a full state from slides & resources.
+ *
+ * @param  {array}  slides - Slides list.
+ * @return {object}        - Quinoa state object.
+ */
+function createQuinoaState(slides = []) {
+  return {
+    editor: createEditorState(slides)
+  };
+}
+
+const QUINOA_DEFAULT_STATE = createQuinoaState([
+  createSlide({title: 'First slide'}),
+  createSlide({title: 'Second slide'})
+]);
+
+const quinoa = new Quinoa({
+  defaultState: QUINOA_DEFAULT_STATE,
+  slideCreator: createSlide
+});
+window.quinoa = quinoa;
 
 const mountNode = document.getElementById('mount');
 
