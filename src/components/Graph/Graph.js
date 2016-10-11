622609d565163410b59b6e479296bd63a849f970
Robin de Mourat <robin.demourat@gmail.com>
2016-10-11 09:02:33 +0200

    setup basic visualizations components structure

---
+++ src/components/Graph/Graph.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Graph.scss';
+
+const Graph = ({
+  data=[],
+  invariantParameters=[]
+}) => {
+  return (
+    <div>
+      <h2>Graph visualization</h2>
+      <p>Invariant parameters:</p>
+      <ul>
+        {invariantParameters.map((parameter, key) => 
+          (<li key={key}>{parameter.id} : {parameter.mappedField}</li>)
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Graph;
+
+
+
+
---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Timeline.scss';
+
+const Timeline = ({
+  data=[],
+  invariantParameters=[]
+}) => {
+  return (
+    <div>
+      <h2>Timeline visualization</h2>
+      <p>Invariant parameters:</p>
+      <ul>
+        {invariantParameters.map((parameter, key) => 
+          (<li key={key}>{parameter.id} : {parameter.mappedField}</li>)
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Timeline;
+
+
+
+
