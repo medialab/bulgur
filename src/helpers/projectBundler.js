d86b7cce47c8ef2dc4b9e308a3fb5b969e736205
Robin de Mourat <robin.demourat@gmail.com>
2016-10-26 12:03:48 +0200

    create html project bundler

--- src/helpers/projectBundler.js
+++ src/helpers/projectBundler.js
@@ -1,5 +1,5 @@
 
-export default function bundleProject (visualizationData, quinoaStory) {
+export function bundleProjectAsJson (visualizationData, quinoaStory) {
   return {
       data: visualizationData.data,
       globalParameters: {
@@ -23,3 +23,48 @@ export default function bundleProject (visualizationData, quinoaStory) {
       }
   };
 }
+
+export function bundleProjectAsHtml (jsonBundle) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bulgur</title>
+  <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre" rel="stylesheet">
+  <meta charset="utf-8">
+</head>
+<body>
+  <div id="mount"></div>
+
+  <!-- Project data -->
+  <script type="text/javascript">
+  window.__project__ = ${JSON.stringify(jsonBundle)}
+  </script>
+
+  <!-- Loading Sigma Dependencies -->
+  <script type="text/javascript" src="https://rawgit.com/medialab/bulgur/master/external/sigma.min.js"></script>
+  <script type="text/javascript" src="https://rawgit.com/medialab/bulgur/master/external/sigma.parsers.gexf.min.js"></script>
+  <script type="text/javascript" src="https://rawgit.com/medialab/bulgur/master/external/sigma.layout.forceAtlas2.min.js"></script>
+  <script type="text/javascript" src="https://rawgit.com/medialab/bulgur/master/external/sigma.plugins.saveCamera.js"></script>
+
+  <!-- App code -->
+  <script type="text/javascript" src="https://rawgit.com/medialab/bulgur/master/build/bundle.js">
+  </script>
+
+</body>
+</html>
+`;
+}
+
+/*
+<!-- Loading Sigma Dependencies -->
+  <script type="text/javascript">${sigmaMain}</script>
+  <script type="text/javascript">${sigmaGexf}</script>
+  <script type="text/javascript">${sigmaForceAtlas2}</script>
+  <script type="text/javascript">${sigmaSaveCamera}</script>
+
+  <!-- App code -->
+  <script type="text/javascript">${bundle}</script>
+*/
