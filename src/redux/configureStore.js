e743e26727cdc5605dae95034a4c6a6adf1b13a1
Robin de Mourat <robin.demourat@gmail.com>
2016-11-07 11:55:59 +0100

    connect app to pouchdb close #21

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,25 +1,38 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import promiseMiddleware from './promiseMiddleware';
+import { persistentStore } from 'redux-pouchdb';
+import { persistentReducer } from 'redux-pouchdb';
+
+const PouchDB = require('pouchdb');
+
+const db = new PouchDB('bulgur');
 
 export default function configureStore (initialState = {}) {
   // Compose final middleware and use devtools in debug environment
   const middleware = applyMiddleware(
     thunk,
-    promiseMiddleware(),
+    promiseMiddleware()
   );
+
   // Create final store and subscribe router in debug env ie. for devtools
-  const store = middleware(createStore)(
-    rootReducer,
-    initialState,
+  const createStoreWithMiddleware = compose(
+    middleware,
+    persistentStore(db),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )(createStore);
+
+  const persistedReducer = persistentReducer(rootReducer);
+
+  const store = createStoreWithMiddleware(
+    persistedReducer, 
+    initialState
   );
 
   if (module.hot) {
     module.hot.accept('./rootReducer', () => {
       const nextRootReducer = require('./rootReducer').default;
-
       store.replaceReducer(nextRootReducer);
     });
   }
