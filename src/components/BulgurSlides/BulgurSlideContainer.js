0c195b67d16fe0cb713e804bbfda347095b317c9
Robin de Mourat <robin.demourat@gmail.com>
2017-02-04 17:34:44 +0100

    refactor slides container as independant container

---
+++ src/components/BulgurSlides/BulgurSlideContainer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const BulgurSlidesContainer = ({
+  activePresentation,
+  activeSlideId,
+  setActiveSlide,
+  addSlide,
+  updateSlide,
+  removeSlide
+}) => {
+  return (
+    <ul className="bulgur-slides">
+      {
+        activePresentation
+        .order
+        .map(slideKey => {
+            const slide = activePresentation.slides[slideKey];
+            const onRemove = () => removeSlide(slideKey);
+            const onGlobalClick = () => setActiveSlide(slideKey, slide);
+
+            const onTitleChange = (e) => {
+              updateSlide(slideKey, {
+                ...slide,
+                title: e.target.value
+              });
+            };
+            const onTextChange = (e) => {
+              updateSlide(slideKey, {
+                ...slide,
+                markdown: e.target.value,
+                draft: undefined
+              });
+            };
+            return (
+              <li className="bulgur-slide" onClick={onGlobalClick} key={slideKey}>
+                <h3>
+                  <input
+                    placeholder="slide title"
+                    type="text"
+                    value={slide.title}
+                    onChange={onTitleChange} />
+                </h3>
+                <div>
+                  <textarea
+                    type="text"
+                    placeholder="slide comment"
+                    value={slide.markdown}
+                    onChange={onTextChange} />
+                </div>
+                <button onClick={onRemove}>Remove slide</button>
+                {activeSlideId === slideKey ? 'active' : ''}
+              </li>
+            );
+          }
+          )
+      }
+      <li>
+        <button type="button" onClick={addSlide}>Add slide</button>
+      </li>
+    </ul>
+  );
+
+};
+
+export default BulgurSlidesContainer;
--- src/features/Bulgur/components/AsideViewLayout.js
+++ src/features/Bulgur/components/AsideViewLayout.js
@@ -4,6 +4,8 @@ import './AsideViewLayout.scss';
 
 // import {EditorComponent} from '../../../helpers/configQuinoa';
 
+import Slides from '../../../components/BulgurSlides/BulgurSlideContainer';
+
 import 'codemirror/lib/codemirror.css';
 import '../../../lib/code-mirror-theme.scss';
 
@@ -24,57 +26,15 @@ const AsideViewLayout = ({
     <h1>
       <button onClick={openSettings} type="button"><img className="bulgur-icon-image" src={require('../assets/settings.svg')} /> {activePresentation.metadata && activePresentation.metadata.title && activePresentation.metadata.title.length ? activePresentation.metadata.title : 'untitled presentation'}</button>
     </h1>
-    {/* <button onClick={returnToLanding} type="button"><img className="bulgur-icon-image" src={require('../assets/landing.svg')} />New presentation / import</button> */}
-    {/*<EditorComponent />*/}
-    <ul className="slides-container">
-      {
-        activePresentation.order &&
-        activePresentation
-          .order.map((slideKey) => {
-            const onRemove = () => removeSlide(slideKey);
-            const slide = activePresentation.slides[slideKey];
-            const onGlobalClick = () => setActiveSlide(slideKey, slide);
-
-            const onTitleChange = (e) => {
-              updateSlide(slideKey, {
-                ...slide,
-                title: e.target.value
-              });
-            };
-            const onTextChange = (e) => {
-              updateSlide(slideKey, {
-                ...slide,
-                markdown: e.target.value,
-                draft: undefined
-              });
-            };
-            return (
-              <li onClick={onGlobalClick} key={slideKey}>
-                <h3>
-                  <input 
-                    placeholder="slide title" 
-                    type="text" 
-                    value={slide.title} 
-                    onChange={onTitleChange} />
-                </h3>
-                <div>
-                  <textarea 
-                    type="text"
-                    placeholder="slide comment"
-                    value={slide.markdown}
-                    onChange={onTextChange} />
-                </div>
-                <button onClick={onRemove}>Remove slide</button>
-                {activeSlideId === slideKey ? 'active' : ''}
-              </li>
-            );
-          }
-          )
-      }
-      <li>
-        <button type="button" onClick={addSlide}>Add slide</button>
-      </li>
-    </ul>
+    {
+      activePresentation.order ?
+        <Slides
+          activePresentation={activePresentation}
+          activeSlideId={activeSlideId}
+          setActiveSlide={setActiveSlide}
+          addSlide={addSlide}
+          updateSlide={updateSlide}
+          removeSlide={removeSlide} /> : null}
     <button type="button" onClick={returnToLanding}>← Home</button>
   </aside>
 );
