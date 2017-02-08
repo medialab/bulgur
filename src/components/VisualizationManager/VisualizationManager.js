2ac5cff2c1fdcecff07ea59bdc66eb6d4b5c7d3e
Robin de Mourat <robin.demourat@gmail.com>
2017-02-08 17:43:23 +0100

    draft editor small draft improvement

---
+++ src/components/VisualizationManager/VisualizationManager.js
@@ -0,0 +1,107 @@
+/**
+ * This module provides a reusable visualization manager, focused on the performance
+ * optimizations related to re-mapping the data / re-rendering the vis
+ * @module bulgur/components/VisualizationManager
+ */
+import React, {Component, PropTypes} from 'react';
+
+import {
+  Timeline,
+  Network,
+  Map,
+  mapMapData,
+  mapTimelineData,
+  mapNetworkData
+} from 'quinoa-vis-modules';
+
+import './VisualizationManager.scss';
+
+class VisualizationManager extends Component {
+
+  constructor(props) {
+    super(props);
+    this.updateData = this.updateData.bind(this);
+
+    this.state = {
+      data: this.updateData(props)
+    }
+  }
+
+  updateData(props) {
+    let visData;
+    const {
+      data,
+      dataMap,
+      visualizationType
+    } = props;
+
+    switch (visualizationType) {
+      case 'map':
+        visData = mapMapData(data, dataMap);
+        break;
+      case 'network':
+        visData = mapNetworkData(data, dataMap);
+        break;
+      case 'timeline':
+        visData = mapTimelineData(data, dataMap);
+        break;
+      default:
+        return null;
+    }
+    console.log('remapping the data', visData);
+    this.setState({
+      data: visData
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (JSON.stringify(this.props.data) !== JSON.stringify(nextProps.data) 
+      || JSON.stringify(this.props.dataMap) !== JSON.stringify(nextProps.dataMap)) {
+      
+      this.updateData(nextProps);
+    }
+  }
+
+  render() {
+    const {
+      visualizationType,
+      viewParameters,
+      onUserChange
+    } = this.props;
+    const {
+      data = []
+    } = this.state;
+    switch (visualizationType) {
+        case 'map':
+          return (<Map
+            allowUserViewChange
+            data={data}
+            onUserViewChange={onUserChange}
+            viewParameters={viewParameters} />);
+        case 'network':
+          return (<Network
+            allowUserViewChange
+            data={data}
+            onUserViewChange={onUserChange}
+            viewParameters={viewParameters} />);
+        case 'timeline':
+          return (<Timeline
+            allowUserViewChange
+            data={data}
+            onUserViewChange={onUserChange}
+            viewParameters={viewParameters} />);
+        default:
+          return null;
+      }
+  }
+}
+
+VisualizationManager.propTypes = {
+  // dataMap: PropTypes.Object
+  // data: PropTypes.Object,
+  // viewParameters: PropTypes.Object,
+  visualizationType: PropTypes.String,
+  onUserChange: PropTypes.func
+};
+
+export default VisualizationManager;
