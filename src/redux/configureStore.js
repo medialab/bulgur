9899b9e035ca2fae065919925595e936be25e501
Robin de Mourat <robin.demourat@gmail.com>
2017-02-20 09:13:37 +0100

    plug redux devtools in the right place and thus fix #73

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -29,18 +29,17 @@ export default function configureStore (initialState = {}) {
   );
 
   // Create final store and subscribe router in debug env ie. for devtools
-  const createStoreWithMiddleware = compose(
+  const createStoreWithMiddleware = window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(
     // related middlewares
     middleware,
     // connections to pouchdb
-    persistentStore(db)
-  )(createStore);
+    persistentStore(db),
+    // connection to redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION__())(createStore) : compose(middleware, persistentStore(db))(createStore);
 
   const store = createStoreWithMiddleware(
     rootReducer,
     initialState,
-    // connection to redux dev tools
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   // live-reloading handling
   if (module.hot) {
