e02a011bbb2ff6e0eee225b6559d22af20f28fc0
Robin de Mourat <robin.demourat@gmail.com>
2017-03-07 18:17:34 +0100

    color: recognized 'color' field used as category

--- src/helpers/colorHelpers.js
+++ src/helpers/colorHelpers.js
@@ -12,27 +12,38 @@ export function generateColorsMap (collection = [], propertyName) {
   const palette = scheme.from_hue(21).scheme('triade').colors();
   let paletteIndex = 1;
   const values = uniq(collection.map(obj => obj[propertyName]));
-  if (values.length < 12) {
-   return collection.reduce((cMap, obj) => {
-    const entry = obj[propertyName];
-    if (cMap[entry] === undefined) {
-      paletteIndex++;
-      cMap[entry] = paletteIndex < palette.length ? '#' + palette[paletteIndex] : '#' + palette[0];
-    }
-    return cMap;
-   }, {
-    default: '#' + palette[0]
-   });
-  }
- else {
-    const vLength = values.length;
-    const scale = chroma.scale(['red', 'blue']);
-    return values.reduce((pal, value, index) => ({
+
+  if (propertyName === 'color' || propertyName === 'couleur') {
+    return values.reduce((pal, value) => ({
       ...pal,
-      [value]: scale(index / vLength).hex()
-    })
-    , {
+      [value]: chroma(value).hex()
+    }), {
       default: '#000000'
     });
+  }
+ else {
+    if (values.length < 12) {
+     return collection.reduce((cMap, obj) => {
+      const entry = obj[propertyName];
+      if (cMap[entry] === undefined) {
+        paletteIndex++;
+        cMap[entry] = paletteIndex < palette.length ? '#' + palette[paletteIndex] : '#' + palette[0];
+      }
+      return cMap;
+     }, {
+      default: '#' + palette[0]
+     });
+    }
+    else {
+      const vLength = values.length;
+      const scale = chroma.scale(['red', 'blue']);
+      return values.reduce((pal, value, index) => ({
+        ...pal,
+        [value]: scale(index / vLength).hex()
+      })
+      , {
+        default: '#000000'
+      });
+    }
   }
 }
